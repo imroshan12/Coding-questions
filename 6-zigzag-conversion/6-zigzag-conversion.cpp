1895e73dac4dc2877e28b1da0b3f4930d087a32a
Sarvesh Roshan <63512093+imroshan12@users.noreply.github.com>
2022-09-01 15:03:48 +0530

    Time: 15 ms (63.97%), Space: 8.3 MB (67.51%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -3,30 +3,23 @@ public:
     string convert(string s, int numRows) {
         if (numRows == 1)
             return s;
-        int n = s.size(), c = 0;
-        vector<vector<char>> zig(1000, vector<char> (1000));
-        int i = 0, j = 0;
+        int n = s.size();
+        int pos = 0;
         string ans = "";
-        while (c < n) {
-            for (i = 0; i < (numRows - 1) && c < n; i++) {
-                // cout << i << " " << j << endl;
-                zig[i][j] = s[c++];
-            }
-            for (i = (numRows - 1); i > 0 && c < n; i--, j++) {
-                // cout << i << " " << j << endl;
-                zig[i][j] = s[c++];
-            }
-            // cout << i << " " << j << endl;
-        }
-        // cout << c << endl;
         for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < 1000; j++) {
-                // cout << zig[i][j] << " ";
-                if (zig[i][j])
-                    ans += zig[i][j];
+            int first = 2*(numRows-1-i), second = 2*(i-0);
+            int c = 0, pos = i;
+            while (pos < n) {
+                ans += s[pos];
+                if (i == 0 || i == numRows - 1) {
+                    pos += max(first, second);
+                } else if (c & 1) {
+                    pos += second;
+                } else {
+                    pos += first;
+                }
+                c++;
             }
-            
-            // cout << endl;
         }
         return ans;
     }
