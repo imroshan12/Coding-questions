76f5cb6e2e3f9cef8ce91ffdd6e2b57b7deb613c
Sarvesh Roshan <63512093+imroshan12@users.noreply.github.com>
2022-09-12 19:59:41 +0530

    Time: 6 ms (84.10%), Space: 10.6 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -5,19 +5,6 @@ public:
         sort(tokens.begin(), tokens.end());
         int score = 0, maxScore = 0, i = 0, j = n-1;
         while (i <= j) {
-            // while (i <= j and power >= tokens[i]) {
-            //     power -= tokens[i++];
-            //     score++;
-            //     maxScore = max(maxScore, score);
-            // }
-            // while (j > i and score > 0) {
-            //     score--;
-            //     power += tokens[j--];
-            //     if (power >= tokens[i])
-            //         break;
-            // }
-            // if (i <= j and power < tokens[i] and score == 0)
-            //     return maxScore;
             if (power >= tokens[i]) {
                 power -= tokens[i++];
                 score++;
