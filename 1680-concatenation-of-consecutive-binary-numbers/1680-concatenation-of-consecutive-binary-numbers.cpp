dbf9d5a127557114d061f22fce88a402934e60f0
Sarvesh Roshan <63512093+imroshan12@users.noreply.github.com>
2022-09-23 20:19:11 +0530

    Time: 992 ms (18.48%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -10,26 +10,19 @@ public:
         }
         return len;
     }
-    long long getFactor(long long len, long long prevFactor) {
-        // long long n = 1;
-        // for (long long i = prevLen; i <= len; i++) {
-        //     n = (n * 2) % MOD;
-        // }
-        // return n;
-        for (long long i = 1; i <= len; i++) {
+    long long getFactor(int len, long long prevFactor) {
+        for (int i = 1; i <= len; i++) {
             prevFactor = (prevFactor * 2) % MOD;
         }
         return prevFactor;
     }
     int concatenatedBinary(int n) {
-        string s = "";
-        long long pos = 0, prevFactor = 1, ans = 0;
+        int binaryLen = 0;
+        long long prevFactor = 1, ans = 0;
         for (long long i = n; i >= 1; i--) {
-            // cout << getFactor(pos, prevFactor) << " ";
-            prevFactor = getFactor(pos, prevFactor); 
+            prevFactor = getFactor(binaryLen, prevFactor); 
             ans = (ans + i*prevFactor) % MOD;
-            // prevPos = pos;
-            pos = findLen(i);
+            binaryLen = findLen(i);
         }
         return (int)ans;
     }
