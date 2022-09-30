9d47a067dd7c8a5b3dab25fcf8eb581e4725037a
Sarvesh Roshan <63512093+imroshan12@users.noreply.github.com>
2022-09-30 22:40:02 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (70.81%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,33 +1,128 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
         vector<int> ans;
-        if (!root) return ans;
         
         queue<TreeNode*> adr;
-        TreeNode* temp;
-        
         adr.push(root);
         adr.push(nullptr);
         
-        while (adr.size()) {
+        TreeNode * temp;
+        
+        while (!adr.empty()) {
             temp = adr.front();
             adr.pop();
-            if (temp) {
-                if (adr.front() == nullptr) 
-                    ans.push_back(temp->val);
-                
-                if (temp->left)
-                    adr.push(temp->left);
-                
-                if (temp->right)
-                    adr.push(temp->right);
-                
+            
+            if (temp != nullptr) {
+                if (adr.front() == nullptr) ans.push_back(temp->val);
+                if (temp->left) adr.push(temp->left);
+                if (temp->right) adr.push(temp->right);
             } else {
-                if (adr.size()) 
+                if (!adr.empty())
                     adr.push(nullptr);
             }
         }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> ans;
+//         if (!root) return ans;
+        
+//         queue<TreeNode*> adr;
+//         TreeNode* temp;
+        
+//         adr.push(root);
+//         adr.push(nullptr);
+        
+//         while (adr.size()) {
+//             temp = adr.front();
+//             adr.pop();
+//             if (temp) {
+//                 if (adr.front() == nullptr) 
+//                     ans.push_back(temp->val);
+                
+//                 if (temp->left)
+//                     adr.push(temp->left);
+                
+//                 if (temp->right)
+//                     adr.push(temp->right);
+                
+//             } else {
+//                 if (adr.size()) 
+//                     adr.push(nullptr);
+//             }
+//         }
+//         return ans;
+//     }
+// };
